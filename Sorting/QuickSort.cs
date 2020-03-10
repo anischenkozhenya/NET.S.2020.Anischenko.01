61d6892f696d10ccd4e4f864cd1fc76b6f902d0f
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2020-03-10 23:13:00 +0300

    +tests +nunit nuget

---
+++ Sorting/QuickSort.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public class QuickSort
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public int[] QuickSorting(int[] array)
+        {
+            var result=new int[array.Length];
+
+
+            return result;
+        }
+    }
+}
---
+++ Tests/QuickSortTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using Sorting;
+
+namespace Tests
+{
+    [TestFixture]
+    public class QuickSortTests
+    {
+        [Test]
+        public void FullInformation()
+        {
+            var expected = new int[] {8,3,2,4,5,6,1,7};
+            var actual = new int[] {1, 2, 3, 4, 5, 6, 7, 8};
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
