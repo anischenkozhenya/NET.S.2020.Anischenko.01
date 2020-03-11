9a01b88102fdcf2a0672d2c38ac5ca972fa0c0f0
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2020-03-11 19:44:02 +0300

    add quicksortmethod

--- Tests/QuickSortTests.cs
+++ Tests/QuickSortTests.cs
@@ -1,6 +1,5 @@
-﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
+﻿using System;
+using NUnit.Framework;
 using Sorting;
 
 namespace Tests
@@ -9,12 +8,26 @@ namespace Tests
     public class QuickSortTests
     {
         [Test]
-        public void FullInformation()
+        public void EvenArray()
         {
-            var expected = new int[] {8,3,2,4,5,6,1,7};
-            var actual = new int[] {1, 2, 3, 4, 5, 6, 7, 8};
-
+            var unSortedArray = new int[] { 8, 3, 2, 4, 5, 6, 1, 7 };
+            var expected = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            var actual = QuickSort.QuickSortMethod(unSortedArray);
             Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void AddArray()
+        {
+            var unSortedArray = new int[] { 8, 3, 2, 5, 6, 1, 7 };
+            var expected = new int[] { 1, 2, 3, 5, 6, 7, 8 };
+            var actual = QuickSort.QuickSortMethod(unSortedArray);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void NullReference()
+        {
+            int[] arrayWithoutRef = null;
+            Assert.Throws<NullReferenceException>(() => QuickSort.QuickSortMethod(arrayWithoutRef));
         }
     }
 }
